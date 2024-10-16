d16944ce225f6f94c3eb34a81e80427377200c2d
Stefan Strand <stefanboytchev@hotmail.com>
2024-10-16 13:04:14 +0200

    fixed merge conflicts and cleaned up

--- homeapp/models.py
+++ homeapp/models.py
@@ -2,14 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-<<<<<<< Updated upstream
-class SmartDevice(models.Model):
-    name = models.CharField(max_length=128)
-   # room = models.ForeignKey(Room, on_delete=models.CASCADE, blank=True, null=True)
-    owner = models.ForeignKey("auth.User", on_delete=models.CASCADE, null=True)
-    description = models.TextField(blank=True)
-    is_on = models.BooleanField(default=False)
-=======
 
 class SmartDevice(models.Model):
     name = models.CharField(max_length=128)
@@ -25,7 +17,6 @@ class SmartDevice(models.Model):
 class Room(models.Model):
     name = models.CharField(max_length=128, unique=False)
     home = models.ForeignKey(Home, on_delete=models.CASCADE, blank=True, null=True)
->>>>>>> Stashed changes
 
     def __str__(self):
         return self.name
@@ -36,31 +27,20 @@ class CarCharger(SmartDevice):
     power_consumption = models.IntegerField(default=0)
     total_power_consumption = models.IntegerField(default=0)
 
-<<<<<<< Updated upstream
-=======
 
 class SmartBulb(SmartDevice):
     brightness = models.IntegerField(default=100)
     color = models.CharField(max_length=20, default='white')
 
 
->>>>>>> Stashed changes
 class SmartThermostat(SmartDevice):
     temperature_in_room = models.IntegerField()
     set_temperature = models.IntegerField()
     humidity = models.IntegerField()
 
     def get_temperature(self):
-<<<<<<< Updated upstream
         #  Call stub to get a temp reading?
         return self.temperature_in_room
 
     def set_temperature(self, temperature):
         self.set_temperature = temperature
-=======
-        # Call stub to get a temp reading?
-        return self.temperature_in_room
-
-    def set_temperature(self, temperature):
-        self.set_temperature = temperature
->>>>>>> Stashed changes
