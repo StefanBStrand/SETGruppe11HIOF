3bfc9ffd1cf38ed75d0eea2ec5f91ad5dbbb9bbb
Stefan Strand <stefanboytchev@hotmail.com>
2024-10-17 07:54:32 +0200

    added Home Class and imported User from django

--- homeapp/models.py
+++ homeapp/models.py
@@ -1,6 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Home(models.Model):
+    name = models.CharField(max_length=128)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.owner.username})"
 
 
 class Room(models.Model):
@@ -9,21 +17,14 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class SmartDevice(models.Model):
     name = models.CharField(max_length=128)
     room = models.ForeignKey('Room', on_delete=models.CASCADE, blank=True, null=True)
-    owner = models.ForeignKey("auth.User", on_delete=models.CASCADE, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     description = models.TextField(blank=True)
     is_on = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.name
-
-
-class Room(models.Model):
-    name = models.CharField(max_length=128, unique=False)
-    home = models.ForeignKey(Home, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -33,7 +34,6 @@ class CarCharger(SmartDevice):
     is_connected_to_car = models.BooleanField(default=False)
     power_consumption = models.IntegerField(default=0)
     total_power_consumption = models.IntegerField(default=0)
-
 
 
 class SmartBulb(SmartDevice):
@@ -52,4 +52,3 @@ class SmartThermostat(SmartDevice):
 
     def set_temperature(self, temperature):
         self.set_temperature = temperature
-
