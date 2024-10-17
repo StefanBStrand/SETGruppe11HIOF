6e6dcca65e5da15950afe35e4fad244e715be9b7
Hansen-Sebastian <sebasth@hiof.no>
2024-10-17 20:37:17 +0200

    Update models.oy
    Changed from User to auth.user to use Djangos standard user.
    
    SmartDevice now abstract

--- homeapp/models.py
+++ homeapp/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
 # Create your models here.
 
 
 class Home(models.Model):
     name = models.CharField(max_length=128)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    owner = models.ForeignKey("auth.User", on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f"{self.name} ({self.owner.username})"
@@ -21,10 +21,13 @@ class Room(models.Model):
 
 class SmartDevice(models.Model):
     name = models.CharField(max_length=128)
-    room = models.ForeignKey('Room', on_delete=models.CASCADE, blank=True, null=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, blank=True, null=True)
+    owner = models.ForeignKey("auth.User", on_delete=models.CASCADE, null=True)
     description = models.TextField(blank=True)
     is_on = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
 
     def __str__(self):
         return self.name
