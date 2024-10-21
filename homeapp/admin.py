d7eab3069cad860f471f8c14c503c5b030d46dc7
Hansen-Sebastian <sebasth@hiof.no>
2024-10-21 14:48:41 +0200

    Update admin.py

--- homeapp/admin.py
+++ homeapp/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from models import Home, Room, CarCharger, SmartDevice, SmartThermostat, SmartBulb
+from .models import Home, Room, CarCharger, SmartDevice, SmartThermostat, SmartBulb
 
+admin.site.register(Home)
+admin.site.register(Room)
+admin.site.register(CarCharger)
+admin.site.register(SmartDevice)
+admin.site.register(SmartThermostat)
+admin.site.register(SmartBulb)
