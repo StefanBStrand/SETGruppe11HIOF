cbe5855c08874b9cdfece9c849a0b08fd137dcc3
Hansen-Sebastian <sebasth@hiof.no>
2024-11-15 21:01:55 +0100

    Revert "Merge branch 'dev' into feature_create_update_delete_SmartBulb"
    This reverts commit 791661523cd7c9300fb1d66446533b63d391d18f, reversing
    changes made to ad477f9f5a29d6e9a75369adf51054322a1b1c1b.

--- homeapp/models.py
+++ homeapp/models.py
@@ -1,14 +1,32 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
-from .services import *
 # Create your models here.
 
 
 class Home(models.Model):
     name = models.CharField(max_length=128)
     owner = models.ForeignKey("auth.User", on_delete=models.CASCADE, null=True)
+    CITY_CHOICES = [
+        ('Fredrikstad', 'Fredrikstad'),
+        ('Sarpsborg', 'Sarpsborg'),
+        ('Nesodden', 'Nesodden'),]
+    city = models.CharField(max_length=128, choices=CITY_CHOICES ,blank=False, default='Fredrikstad')
+    lat = models.FloatField(default=59.21)
+    lon = models.FloatField(default=10.92)
+
+    if city == 'Fredrikstad':
+        lat = 59.21
+        lon = 10.92
+    elif city == 'Sarpsborg':
+        lat = 59.28
+        lon = 11.11
+    elif city == 'Nesodden':
+        lat = 59.84
+        lon = 10.58
+
 
     def __str__(self):
-        return f"{self.name} ({self.owner.username})"
+        return f"{self.name}"
 
 
 class Room(models.Model):
@@ -20,12 +38,19 @@ class Room(models.Model):
 
 
 class SmartDevice(models.Model):
-    device_type = models.CharField(max_length=50, null=True, blank=True)
     name = models.CharField(max_length=128)
     room = models.ForeignKey(Room, on_delete=models.CASCADE, blank=True, null=True)
     owner = models.ForeignKey("auth.User", on_delete=models.CASCADE, null=True)
     description = models.TextField(blank=True)
     is_on = models.BooleanField(default=False)
+    device_type = models.CharField(max_length=50, default='smartdevice')
+
+    def save(self, *args, **kwargs):
+        # Bare sett device_type hvis det ikke allerede er satt
+        if not self.device_type:
+            self.device_type = self.DEVICE_TYPE  # Bruk konstanten fra underklassen
+        super().save(*args, **kwargs)
+
 
     def __str__(self):
         return self.name
@@ -39,60 +64,49 @@ class CarCharger(SmartDevice):
     max_power_output = models.IntegerField(default=60)
     power_consumption = models.IntegerField(default=0)
     total_power_consumption = models.IntegerField(default=0)
+    device_type = 'carcharger'
 
-    def fetch_data(self):
-        # Fetches car charger data from a stub simulating the external system.
-        # Updates local status if a change is detected.
-        data = fetch_carcharger_data_from_external_system()
-        if data["response"] == "success":
-            # Update connection status if changed
-            if data["is_connected_to_car"] != self.is_connected_to_car:
-                self.is_connected_to_car = data["is_connected_to_car"]
 
-            # Update other fields based on the fetched data
-            self.car_battery_capacity = data["car_battery_capacity"]
-            self.car_battery_charge = data["car_battery_charge"]
-            self.is_charging = data["is_charging"]
+    def get_device_type(self):
+        return "carcharger"
+
+
+
+
+    def connect_to_car(self):
+        if not self.is_connected_to_car:
+            self.is_connected_to_car = True
             self.save()
-            return "Data fetched and local status updated successfully."
-        return "Failed to fetch data from the external system."
+            return "Car is now connected."
+        return "Car is already connected."
+
+    def disconnect_from_car(self):
+        if self.is_connected_to_car:
+            self.is_connected_to_car = False
+            self.save()
+            return "Car is now disconnected."
+        return "Car is already disconnected."
 
     def start_charging(self, power_rate):
-        # Sends a start charging command to the external system.
-        # Updates local charging status on success.
-        if not self.is_connected_to_car:
-            return "Car is not connected. Cannot start charging."
-        
-        if power_rate == 0:
-            return "Cannot start charging with 0 kwh. Choose valid power rate"
-        
-        if power_rate > self.max_power_output:
-            return "Power rate exceeds maximum output capacity."
 
-        response = send_charging_status_to_external_system(power_rate, start=True)
-        if response["response"] == "success":
-            self.power_consumption = power_rate  # Update local power consumption
-            self.is_charging = True  # Mark as charging
+        if not self.is_connected_to_car:
+            raise ValueError("Car is not connected. Please connect the car to start charging.")
+        if power_rate <= self.max_power_output:
+            self.power_consumption = power_rate
+            self.is_charging = True
             self.save()
-            return f"Charging started at {power_rate} kW."
-        return "Failed to start charging."
+            return "Charging started at {} kW.".format(power_rate)
 
     def stop_charging(self, charging_minutes):
-        # Sends a stop charging command to the external system.
-        # Updates local charging status on success.
-        if not self.is_connected_to_car or not self.is_charging:
-            return "No active charging session to stop."
 
-        response = send_charging_status_to_external_system(0, start=False)
-        if response["response"] == "success":
-            # Calculate total power consumed during the charging session
-            total_consumed = self.power_consumption * charging_minutes / 60
-            self.total_power_consumption += total_consumed  # Update total power consumption
-            self.power_consumption = 0  # Reset current power consumption
-            self.is_charging = False  # Mark as not charging
+        if self.is_connected_to_car and self.power_consumption > 0 and self.is_charging:
+            self.is_charging = False
+            total_consumed = self.power_consumption * charging_minutes
+            self.total_power_consumption += total_consumed
+            self.power_consumption = 0
             self.save()
-            return f"Charging stopped. Total power consumed: {total_consumed:.2f} kWh."
-        return "Failed to stop charging."
+            return "Charging stopped. Total power consumed: {} kWh.".format(total_consumed)
+        return "No active charging session to stop."
 
     def reset_power_consumption(self):
 
@@ -114,34 +128,6 @@ class CarCharger(SmartDevice):
         charging_time_minutes_to_full = (remaining_capacity / (self.power_consumption)) * 60
         return "Estimated charging time: {:.2f} minutes.".format(charging_time_minutes_to_full)
 
-    # Getters
-    def get_battery_capacity(self):
-        # Returns the total battery capacity of the car.
-        return self.car_battery_capacity
-
-    def get_battery_charge(self):
-        # Returns the current charge level of the car battery.
-        return self.car_battery_charge
-
-    def get_is_connected_status(self):
-        # Returns whether the car is connected to the charger.
-        return self.is_connected_to_car
-
-    def get_is_charging_status(self):
-        # Returns whether the charger is actively charging.
-        return self.is_charging
-
-    def get_max_power_output(self):
-        # Returns the maximum power output of the charger.
-        return self.max_power_output
-
-    def get_current_power_consumption(self):
-        # Returns the current power consumption rate during charging.
-        return self.power_consumption
-
-    def get_total_power_consumption(self):
-        # Returns the total power consumption of the charger.
-        return self.total_power_consumption
 
 class SmartBulb(SmartDevice):
     COLOR_CHOICES = [
@@ -149,85 +135,43 @@ class SmartBulb(SmartDevice):
         ('black', 'Black'),
         ('red', 'Red'),
         ('green', 'Green'),
-        ('yellow', 'Yellow'),
-        ('blue', 'Blue'),
+        ('yellow', 'Yellow')
     ]
 
     brightness = models.IntegerField(default=100)
     color = models.CharField(max_length=20, choices=COLOR_CHOICES, default='white')
+    device_type = 'smartbulb'
+    def get_device_type(self):
+        return "smartbulb"
 
-    def fetch_data(self):
-        # Fetches SmartBulb data from a stub simulating the external system.
-        data = fetch_smartbulb_data_from_external_system()
-        if data:
-            self.is_on = data["is_on"]
-            self.brightness = data["brightness"]
-            self.color = data["color"]
-            self.save()
-        return data
 
-    def update_brightness(self, new_brightness):
-        # Checks if the new brightness value is valid
-        if not (0 <= new_brightness <= 100):
-            return "Invalid brightness value. Must be between 0 and 100."
-        
-        # Updates the brightness of the SmartBulb via a stub simulating the external system.
-        response = send_brightness_update_to_external_system(new_brightness)
-        if response["response"] == "success":
-            self.brightness = response["updated_brightness"]
-            self.save()
-            return f"Brightness updated to {self.brightness}%."
-        return "Failed to update brightness."
-
-    def update_color(self, new_color):
-        # Checks if the new color value is valid
-        if new_color not in dict(self.COLOR_CHOICES):
-            return f"Invalid color value. Must be one of: {', '.join(dict(self.COLOR_CHOICES).keys())}."
-        
-        # Updates the color of the SmartBulb via a stub simulating the external system.
-        response = send_color_update_to_external_system(new_color)
-        if response["response"] == "success":
-            self.color = response["updated_color"]
-            self.save()
-            return f"Color updated to {self.color}."
-        return "Failed to update color."
 
     def turn_on(self):
-        # Turns on the SmartBulb via a stub simulating the external system.
-        response = send_turn_on_to_external_system()
-        if response["response"] == "success":
-            self.is_on = True
-            self.save()
-            return "SmartBulb is now ON."
-        return "Failed to turn on the SmartBulb."
+        """Skrur på lyset"""
+        self.is_on = True
+        print("Turning on.")
 
     def turn_off(self):
-        # Turns off the SmartBulb via a stub simulating the external system.
-        response = send_turn_off_to_external_system()
-        if response["response"] == "success":
-            self.is_on = False
-            self.save()
-            return "SmartBulb is now OFF."
-        return "Failed to turn off the SmartBulb."
-    
-    # Getters
-    def get_brightness(self):
-        # Returns the current brightness level
-        return self.brightness
+        self.is_on = False
+        print("Turning off.")
 
-    def get_color(self):
-        # Returns the current color of the SmartBulb
-        return self.color
+    def set_brightness(self, value):
+        if 0 <= value <= 100:
+            self.brightness = value
+            print(f"Brightness set to {value}%.")
 
-    def get_is_on_status(self):
-        # Returns whether the SmartBulb is currently on
-        return self.is_on
+    def set_color(self, new_color):
+        if new_color in dict(self.COLOR_CHOICES):
+            self.color = new_color
+            print(f"Color set to {new_color}.")
+
 
 
 class SmartThermostat(SmartDevice):
-    temperature_in_room = models.IntegerField()
-    set_temperature = models.IntegerField(default=22)  
-    humidity = models.IntegerField()
+    temperature_in_room = models.IntegerField(blank=True, null=True)
+    set_temperature = models.IntegerField(default=22, validators=[MinValueValidator(5), MaxValueValidator(30)])  # TODO Change field to current_temperature.
+    humidity = models.IntegerField(blank=True, null=True)
+    device_type = 'smartthermostat'
 
     # Adding a mode field with possible choices
     MODE_CHOICES = [
@@ -236,65 +180,17 @@ class SmartThermostat(SmartDevice):
         ('off', 'Off'),
     ]
     mode = models.CharField(max_length=10, choices=MODE_CHOICES, default='off')
-
-    def fetch_data(self):
-        
-        # Fetches thermostat data from a stub simulating the external system. Could run at intervals and look for changes.
-        
-        data = fetch_thermostat_data_from_external_system()
-        if data:
-            self.temperature_in_room = data["current_temperature"]
-            self.set_temperature = data["set_temperature"]
-            self.humidity = data["humidity"]
-            self.mode = data["mode"]
-            self.save()
-        return data
-
-    def update_temperature(self, new_temperature):
-        
-        # Updates the thermostat's temperature via a stub simulating the external system.
-        
-        response = send_temperature_update_to_external_system(new_temperature)
-        if response["response"] == "success":
-            self.set_temperature = response["updated_temperature"]
-            self.save()
-            return f"Temperature updated to {self.set_temperature}°C."
-        return "Failed to update temperature."
-
-    def update_mode(self, new_mode):
-        
-        # Updates the thermostat's mode via a stub simulating the external system.
-        
-        response = send_mode_update_to_external_system(new_mode)
-        if response["response"] == "success":
-            self.mode = response["updated_mode"]
-            self.save()
-            return f"Mode updated to {self.mode}."
-        return "Failed to update mode."
+    def get_device_type(self):
+        return "smartthermostat"
 
     def get_temperature(self):
-        # Returns the current room temperature
         return self.temperature_in_room
 
-    def get_set_temperature(self):
-        # Returns the desired temperature (set point)
-        return self.set_temperature
-
-    def get_humidity(self):
-        # Returns the current humidity level
-        return self.humidity
-
-    def get_mode(self):
-        # Returns the current operating mode of the thermostat
-        return self.mode
-
-
-
-
-        
-    # Send request to Mock-api --> api.update_temperature()
-    # Mock-apiet returnerer True/false basert på om det har gått ok.
-    # Denne metoden returnerer denne true/false videre tilbake til controller (view)
+    def update_temperature(self, temperature):
+        self.set_temperature = temperature
+        # Send request to Mock-api --> api.update_temperature()
+        # Mock-apiet returnerer True/false basert på om det har gått ok.
+        # Denne metoden returnerer denne true/false videre tilbake til controller (view)
 
 
     # TODO: method for view humidity
